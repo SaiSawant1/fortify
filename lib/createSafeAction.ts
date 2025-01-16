088f74222c90e72aa3ef333deb328785a5389cdb
SaiSawant1 <saisawant2003@gmail.com>
2025-01-16 23:50:51 +0530

    safe action

---
+++ lib/createSafeAction.ts
@@ -0,0 +1,27 @@
+import { z } from "zod";
+
+export type FieldErrors<T> = {
+  [k in keyof T]: string[];
+};
+
+export type ActionState<TInput, TOutput> = {
+  fieldErrors?: FieldErrors<TInput>;
+  error?: string;
+  output?: TOutput;
+};
+
+export const CreateSafeAction = <TInput, TOutput>(
+  schema: z.Schema<TInput>,
+  handler: (data: TInput) => Promise<ActionState<TInput, TOutput>>,
+) => {
+  return async (data: TInput): Promise<ActionState<TInput, TOutput>> => {
+    const validatedSchema = schema.safeParse(data);
+    if (!validatedSchema.success) {
+      return {
+        fieldErrors: validatedSchema.error.flatten()
+          .fieldErrors as FieldErrors<TInput>,
+      };
+    }
+    return handler(validatedSchema.data);
+  };
+};
